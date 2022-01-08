70d22dd520c78fa122715cd51e38eea9d435bcf8
Mohammad Azam <azamsharp@gmail.com>
2022-01-08 07:21:34 -0600

    Initial Commit

---
+++ Trips/TripsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripsApp.swift
+//  Trips
+//
+//  Created by Mohammad Azam on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct TripsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
